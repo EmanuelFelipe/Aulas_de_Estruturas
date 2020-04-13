aa95eaa3cf9ccd317b7c8f7178ee1a1ddecf1024
Emanuel Felipe <EmanuelFelipe0501@gmail.com>
2020-04-12 21:45:22 -0300

    exercicio e mais tristeza

--- lista_exercicio/lista_exercicio.c
+++ lista_exercicio/lista_exercicio.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 typedef struct Lista{
+    int qtd_elementos;
     struct Registro * primeiro_elemento;
-    struct Registro * ultimo_elemento;
 }Lista;
 
 
@@ -11,7 +11,6 @@ typedef struct Lista{
 typedef struct Registro{
     int valor;
     struct Registro * proximo_elemento;
-    struct Registro * elemento_anterior;
 }Registro;
 
 Lista * cria_lista();
@@ -21,7 +20,8 @@ void adiciona_lista(Lista * l1, int x);
 void remove_ultimo(Lista * l1);
 void remove_especifico(Lista * l1, int x);
 void mostrar(Lista * l1);
-void verifica_ordem(Lista * l1);
+void verificaOrdem(Lista * l1);
+void mostraPosteriorAnterior(Lista * l1, int x);
 
 void menu(Lista * l1){
     int opcao;
@@ -32,15 +32,14 @@ void menu(Lista * l1){
         printf("1-ADICIONAR\n");
         printf("2-REMOVER ULTIMO\n");
         printf("3-REMOVER ESPECIFICO\n");
-        printf("4-BUSCAR\n");
-        printf("5-MOSTRAR\n");
-        printf("6-ORDEORDENADO\n");
+        printf("4-MOSTRAR\n");
+        printf("5-MOSTRAR POSTERIOR E ANTERIOR\n");
         scanf("%d", &opcao);
 
         switch (opcao)
         {
         case 0:
-           printf("saindo do programa\n");
+            printf("saindo do programa\n");
             break;
         case 1:
             printf("adiciona um valor ai meu patrao\n");
@@ -51,18 +50,18 @@ void menu(Lista * l1){
             remove_ultimo(l1);
             break;
         case 3:
-            printf("qual valor quer remover, ein arrombadinho?\n");
+            printf("qual valor quer remover?\n");
             scanf("%d", &valor);
             remove_especifico(l1, valor);
             break;
-        case 4: 
-            printf("essa funcao nao existe no momento\n");
-            break;
-        case 5:
+        case 4:
             mostrar(l1);
             break;
-        case 6:
-            verifica_ordem(l1);
+        case 5:
+            printf("qual valor quer ver o posterior e anterior?\n");
+            scanf("%d", &valor);
+            mostraPosteriorAnterior(l1, valor);
+            
             break;
         default:
             printf("e quem te disse q essa opcao existe\n");
@@ -82,7 +81,7 @@ Lista * cria_lista(){
     Lista * Retorno = (Lista *)malloc(sizeof(Lista));
 
     Retorno -> primeiro_elemento = NULL;
-    Retorno -> ultimo_elemento = NULL;
+    Retorno -> qtd_elementos = 0;
 
     return Retorno;
 }
@@ -92,7 +91,6 @@ Registro * cria_registro(){
 
     registro_criado->valor = 0;
     registro_criado->proximo_elemento = NULL;
-    registro_criado->elemento_anterior = NULL;
 
     return registro_criado;
 }
@@ -104,56 +102,65 @@ void adiciona_lista(Lista * l1, int x){
     // lista vazia
     if(l1->primeiro_elemento == NULL){
         l1->primeiro_elemento = criado;
-        l1->ultimo_elemento=criado;
+        l1->qtd_elementos++;
         printf("Adicionei meu patrao\n");
+        printf("%d\n", l1->qtd_elementos);
     }else{
-        Registro * aux = l1->ultimo_elemento;
+        Registro * aux = l1->primeiro_elemento;
 
-        l1->ultimo_elemento = criado;
-        criado->elemento_anterior = aux;
-        aux->proximo_elemento = criado;  
+        while(aux->proximo_elemento != NULL){
+            aux = aux->proximo_elemento;
+        }
+        aux->proximo_elemento = criado;
         printf("adicionei mais um meu patrao\n");
+        l1->qtd_elementos++;
     }
 
 }
 void remove_ultimo(Lista * l1){
-    if (l1->ultimo_elemento == NULL){
+    if (l1->primeiro_elemento == NULL){
         printf("tem nada aqui nao, entao nao me enche o saco\n");
     }else{
-        Registro * aux = l1->ultimo_elemento;
+        Registro * aux = l1->primeiro_elemento;
+        Registro * ant = NULL;
 
-        l1->ultimo_elemento = aux->elemento_anterior;
-        aux->elemento_anterior->proximo_elemento = NULL; 
+        while(aux->proximo_elemento->proximo_elemento != NULL){
+            aux = aux->proximo_elemento;
+        }
+        ant = aux;
+        aux = aux->proximo_elemento;
+        ant->proximo_elemento = NULL;
         free(aux);
         printf("apaguei o ultimo aqui, agr vaza\n");
+        l1->qtd_elementos--;
     }
 }
 void remove_especifico(Lista * l1, int x){
 
-    if (l1->ultimo_elemento == NULL){
+    if (l1->primeiro_elemento == NULL){
         printf("tem nada aqui nao, entao nao me enche o saco\n");
     }else{
         Registro * aux = l1->primeiro_elemento;
-        while(aux->proximo_elemento != NULL){
-            if(aux->valor = x){
-                printf("elemento encontrado\n");
-                break;
-            }
+        Registro * ant = NULL;
+        while(aux->valor != x){
+            if(aux->proximo_elemento !=NULL){
+            ant = aux;
             aux = aux->proximo_elemento;
+            }
         }
         if(aux == l1->primeiro_elemento){
             l1->primeiro_elemento = aux->proximo_elemento;
-            aux->proximo_elemento->elemento_anterior = NULL;
             free(aux);
             printf("apagou e se nao queria se foldeu\n");
+            l1->qtd_elementos--;
         }else{
-            if(aux == l1->ultimo_elemento){
+            if(aux->proximo_elemento == NULL){
                 remove_ultimo(l1);
             }else{
-                aux->elemento_anterior->proximo_elemento = aux->proximo_elemento;
-                aux->proximo_elemento->elemento_anterior = aux->elemento_anterior;
+                ant->proximo_elemento = aux->proximo_elemento;
                 free(aux);
                 printf("se foudeu se foudeu o problema é seu (apaguei hehe).\n"); 
+                l1->qtd_elementos--;
             }
         }
     }
@@ -171,26 +178,34 @@ void mostrar(Lista * l1){
         printf("\n");
     }
 }
-void verifica_ordem(Lista * l1){
-    if (l1->ultimo_elemento == NULL){
+void mostraPosteriorAnterior(Lista * l1, int x){
+    if (l1->primeiro_elemento == NULL){
         printf("tem nada aqui nao, entao nao me enche o saco\n");
     }else{
         Registro * aux = l1->primeiro_elemento;
-        while(aux != NULL){
+        Registro * ant = NULL;
 
-            if(aux->elemento_anterior == NULL){
+        while(aux->valor != x){
+            if(aux->proximo_elemento !=NULL){
+            ant = aux;
             aux = aux->proximo_elemento;
-            continue;    
             }
-            if(aux->elemento_anterior->valor>aux->valor){
-                printf("nao, esse trem nao ta crescente\n");
-                break;
-            }
-            aux = aux->proximo_elemento;
         }
-        if(aux->proximo_elemento==NULL){
-            printf("sim, esse trem ta ordenado\n");
-        }
+        printf("%d", aux->proximo_elemento->valor);
+    printf("elemento atual: %d\n", x);
+    if(aux->valor == l1->primeiro_elemento->valor  && l1->qtd_elementos != 1){
+        printf("nao tem anterior\n");
+        printf("posterior: %d\n", aux->proximo_elemento->valor);
+    }if (aux->proximo_elemento == NULL && l1->qtd_elementos !=1){
+        printf("anterior: %d\n", ant->valor);
+        printf("nao tem posterior\n");
+    }if (l1->qtd_elementos == 1){
+        printf("nao tem anterior\n");
+        printf("nao tem posterior\n");
+    }else{
+        printf("anterior: %d\n", ant->valor);
+        printf("posterior: %d\n", aux->proximo_elemento->valor);
     }
+    
 }
-
+}
